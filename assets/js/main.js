a1651ec6ffb22b6d935ebadbc8a0dfe006da3ee0
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-16 00:18:50 -0400

    updated js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,18 @@ console.log("hola");
 //selector languaje
 
 // Name and Email validation Function.
-/*
-function validation() {
-    var name = document.getElementById("name").value;
+document.getElementById('enviar').addEventListener('click',validate);
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (name === '' || email === '') {
-        alert("Please fill all fields...!!!!!!");
-        return false;
+    if (username == " " && password == " ") {
+        alert("Enter parametters");
+
+    } else{
+        alert("muy bien")
     }
-}*/
+
+}
