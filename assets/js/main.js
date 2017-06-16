20837a0949e0d13e3c1faf15f95322190e40930e
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-16 00:02:29 -0400

    updated gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 console.log("hola");
 //selector languaje
+
+// Name and Email validation Function.
+/*
+function validation() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    if (name === '' || email === '') {
+        alert("Please fill all fields...!!!!!!");
+        return false;
+    }
+}*/
---
+++ assets/js/photolog.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+function drag(ev) {
+    console.log(ev.target.src); //verificamos en consola lo que va ocurriendoo
+    ev.dataTransfer.setData('text', ev.target.id); //indicamos el tipo de dato que vamos a insertar
+}
+//iniciallizamos Drop
+function permitirDrop(ev) {
+    //evitamos que el navegador manipule nuestros datos
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    //tomamos el dato que hemos arrastrado
+    var id_foto = ev.dataTransfer.getData('text');
+    //agregamos el elemento arrastrado al elemento en el que se produjo el evento ondrop.
+    ev.target.appendChild(document.getElementById(id_foto));
+}
